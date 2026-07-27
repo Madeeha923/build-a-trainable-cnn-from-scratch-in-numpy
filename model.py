4cef88be4721f261a3d2da753c11d1850e7f94ce
Madeeha Fatima <madeehaarif15@gmail.com>
2026-07-27 12:57:21 +0530

    Step 43: init_linear_layer

--- model.py
+++ model.py
@@ -616,8 +616,21 @@ def init_conv_layer(out_channels, in_channels, kernel_size, seed=0):
 
     return params
 
-# Step 43 - init_linear_layer (not yet solved)
-# TODO: implement
+# Step 43 - init_linear_layer
+def init_linear_layer(in_features, out_features, seed=0):
+    # TODO: return {'W': He-init matrix (in_features, out_features), 'b': zero bias (out_features,)}
+    W = he_init(
+        (in_features, out_features),
+        in_features,
+        seed
+    )
+
+    b = init_zero_bias(out_features)
+
+    return {
+        "W": W,
+        "b": b
+    }
 
 # Step 44 - init_lenet (not yet solved)
 # TODO: implement
